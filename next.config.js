f72d41e51c6fb96af3092384b75a6ab423934d68
David Montoya <deividelpaisa@hotmail.com>
2023-01-22 00:08:32 -0500

    fix: correction of svg image processing with webpack and import errors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
+const path = require('node:path')
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   swcMinify: true,
   reactStrictMode: true,
@@ -10,6 +12,15 @@ const nextConfig = {
   },
   experimental: {
     appDir: true
+  },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+      issuer: /\.tsx$/
+    })
+
+    return config
   }
 }
 
